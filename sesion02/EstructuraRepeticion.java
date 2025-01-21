7429c2ae2c8fff420fcc8ca0282d1fdb1342d948
Jorge Ramón <jorge.ramon2128@gmail.com>
2025-01-20 20:51:51 -0600

    Codigo inicial de la estructura de repeticion

--- sesion02/EstructuraRepeticion.java
+++ sesion02/EstructuraRepeticion.java
@@ -1,5 +1,28 @@
 public class EstructuraRepeticion {
     
     public static void main(String[] args) {
+
+        /*
+         * Las 3 sentencias son totalmente OPCIONALES
+         * for (inicializacion; condicion; incremento/decremento) {
+         * }
+         */
+
+        // Ciclo infinito con for
+        /*for (;;) {
+            System.out.println("Ciclo infinito");
+        }*/
+
+        for (int i = 1; i <= 100; i++) {
+            if (i%10 == 0) {
+                break; // break detiene la ejecución del ciclo
+            }
+
+            if (i%2 == 0) {
+                continue; // continue genera otra ejecución del ciclo
+            }
+
+            System.out.println(i);
+        }
     }
 }
