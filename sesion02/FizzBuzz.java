0d68439aca8a8d3f1dfe4fa5604af2b852fb85ae
Jorge Ramón <jorge.ramon2128@gmail.com>
2025-01-20 20:17:35 -0600

    Solución al problema de FizzBuzz

--- sesion02/FizzBuzz.java
+++ sesion02/FizzBuzz.java
@@ -28,5 +28,15 @@ public class FizzBuzz {
          * 14
          * FizzBuzz (15)
          */
+
+        if (numero%3 == 0 && numero%5 == 0) {
+            System.out.println("FizzBuzz");
+        } else if(numero%3 == 0) {
+            System.out.println("Fizz");
+        } else if (numero%5 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(numero);
+        }
     }
 }
