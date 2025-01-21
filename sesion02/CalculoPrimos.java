cc5922676a203c14ed5d9fb733bdf584cfbd7fa1
Jorge Ramón <jorge.ramon2128@gmail.com>
2025-01-20 21:43:28 -0600

    Solución al cálculo de primos

---
+++ sesion02/CalculoPrimos.java
@@ -0,0 +1,27 @@
+public class CalculoPrimos {
+    /*
+     * Dado un número n, verificar si el número es un número primo
+     * 
+     * Un número es primo es aquel que se divide por si mismo y por la unidad (1).
+     * 
+     * 2, 3, 5, 7, 11, 13, 17, etc...
+     */
+    
+    public static void main(String[] args) {
+        int n = 77;
+        boolean esPrimo = true;
+
+        for (int i = 2; i < n; i++) {
+            if (n%i == 0) {
+                 esPrimo = false;
+                 break;
+            }
+        }
+
+        if (esPrimo) {
+            System.out.println(n + " es primo.");
+        } else {
+            System.out.println(n + " no es primo.");
+        }
+    }
+}
