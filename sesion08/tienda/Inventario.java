26d3eeee115b955e2d5fa6f4d67ac8cd9e283560
Jorge Ramón <jorge.ramon2128@gmail.com>
2025-02-12 21:44:15 -0600

    Convirtiendo un archivo de texto en una lista de objetos

--- sesion08/tienda/Inventario.java
+++ sesion08/tienda/Inventario.java
@@ -1,6 +1,10 @@
 package tienda;
 
+import tienda.modelos.Papas;
+import tienda.modelos.Pastelito;
 import tienda.modelos.Producto;
+import tienda.modelos.Refresco;
+
 import java.util.List;
 import java.util.LinkedList;
 import java.io.File;
@@ -21,19 +25,30 @@ public class Inventario {
             String linea;
 
             while ((linea = bufferedReader.readLine()) != null) {
-                String[] piezas = linea.split("\\|");
+                String[] piezas = linea.split(",");
+
+                String nombre = piezas[1];
 
                 switch(piezas[0]) {
                     case "REFRESCO":
-                        System.out.println("Soy un refresco");
+                        int mililitros = Integer.parseInt(piezas[2]);
+                        int precioRefresco = Integer.parseInt(piezas[3]);
+
+                        inventario.add(new Refresco(nombre, precioRefresco, mililitros));
                     break;
 
                     case "PAPAS":
-                        System.out.println("Soy unas papas");
+                        int peso = Integer.parseInt(piezas[2]);
+                        String sabor = piezas[3];
+                        int precioPapas = Integer.parseInt(piezas[4]);
+
+                        inventario.add(new Papas(nombre, precioPapas, peso, sabor));
                     break;
 
                     case "PASTELITO":
-                        System.out.println("Soy un pastelito");
+                        int precioPastelito = Integer.parseInt(piezas[2]);
+
+                        inventario.add(new Pastelito(nombre, precioPastelito));
                     break;
                 }
             }
