8f8fb8bb1f6ef76fc50bd7397e935b01aa6fe2b6
Jorge Ramón <jorge.ramon2128@gmail.com>
2025-02-12 19:40:24 -0600

    Bloque try-catch

--- sesion08/excepciones/AdivinaNumero.java
+++ sesion08/excepciones/AdivinaNumero.java
@@ -1,35 +1,43 @@
 package excepciones;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class AdivinaNumero {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = null;//new Scanner(System.in);
         int vidas = 5;
 
         // Math.random() da un número entre 0.0000 y 0.999999
         int adivina = (int)(Math.floor(Math.random() * 10)) + 1;
 
         do {
-            System.out.println("\nAdivina un número del 1 al 10 (tienes " + vidas + " intentos restantes)");
-            int numero = sc.nextInt();
-            sc.nextLine();
+            try {
+                System.out.println("\nAdivina un número del 1 al 10 (tienes " + vidas + " intentos restantes)");
+                int numero = sc.nextInt();
+                sc.nextLine();
 
-            if (numero == adivina) {
-                System.out.println("FELICIDADES GANASTE!!!");
-                break;
-            } else if (numero < adivina) {
-                System.out.println("El número es más grande...");
-                vidas--;
-            } else  {
-                System.out.println("El número es más pequeño...");
-                vidas--;
-            }
+                if (numero == adivina) {
+                    System.out.println("FELICIDADES GANASTE!!!");
+                    break;
+                } else if (numero < adivina) {
+                    System.out.println("El número es más grande...");
+                    vidas--;
+                } else  {
+                    System.out.println("El número es más pequeño...");
+                    vidas--;
+                }
 
-            if (vidas == 0) {
-                System.out.println("PERDISTE!! EL NÚMERO ERA: " + adivina);
-                break;
+                if (vidas == 0) {
+                    System.out.println("PERDISTE!! EL NÚMERO ERA: " + adivina);
+                    break;
+                }
+            } catch(InputMismatchException ex) {
+                // Éste bloque se activa únicamente cuando dentro del bloque "try"
+                // se lanza una excepción de tipo "InputMismatchException"
+                System.out.println("\nEL NÚMERO QUE INGRESASTE ES INVÁLIDO...");
+                sc.nextLine();
             }
         } while (true);
     }
